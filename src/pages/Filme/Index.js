67f5bb4038f73891acadb6732cf2d4d79b404f10
Edgar Gomes <edgargomes.iff@gmail.com>
2022-06-20 22:35:08 -0300

    Criados arquivos iniciais e componente de Router.

---
+++ src/pages/Filme/Index.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Filme = ()=>{
+	return(
+		<div>
+			<strong>Bem-vindo ao Filme!</strong>
+		</div>
+		)
+}
+export default Filme;
---
+++ src/pages/Home/Index.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Home = ()=>{
+	return(
+		<div>
+			<strong>Bem-vindo à Home!</strong>
+		</div>
+		)
+}
+export default Home;
